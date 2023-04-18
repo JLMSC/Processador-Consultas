9629a35c610085ce02520011fd519a6288d30514
JLMSC <reecaptcha@gmail.com>
2023-04-18 14:43:05 -0300

    Adicionado nova exceção relacionada a invalidez dos parâmetros FROM.

---
+++ source/Exceptions/invalid_from_params.py
@@ -0,0 +1,25 @@
+"""Arquivo responsável pela exceção relacionada a
+existência de parâmetros incorretos na cláusula FROM"""
+
+class InvalidFromParametersException(Exception):
+    """Exceção lançada quando há parâmetros incorretos
+    na cláusula FROM.
+    """
+
+def raise_invalid_from_params(sql_command: str) -> None:
+    """Lança uma exceção quando há parâmetros incorretos
+    na cláusula FROM.
+
+    Args:
+        sql_command (str): O comando SQL usado para
+        validação dos parâmetros na cláusula FROM.
+
+    Raises:
+        InvalidFromParametersException: Exceção
+        customizada para alertar a existência de
+        parâmetros incorretos na cláusula FROM.
+    """
+    raise InvalidFromParametersException(
+        sql_command +
+        "\nOs parâmetros na cláusula FROM estão incorretos."
+    )
