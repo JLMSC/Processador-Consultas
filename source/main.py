74c45ece663f542db2401f3834e246bbe4f025dd
JLMSC <reecaptcha@gmail.com>
2023-04-26 12:50:28 -0300

    Adicionado exemplo base no arquivo Main.py

--- source/main.py
+++ source/main.py
@@ -1,10 +1,12 @@
 from Parser.parser import Parser
 
-# TODO: Inserir mais casos de teste. (Botar em um Loop)
 Parser(
     "SELECT * " +
     "FROM tabela1 " +
-    "JOIN tabela2 ON tabela1.id = tabela2.id " +
-    "JOIN tabela3 ON tabela2.id = tabela3.id " +
-    "JOIN tabela4 ON tabela3.id = tabela4.id;"
+    "JOIN tabela5 ON id = id " +
+    "JOIN tabela2 ON tabela1.id = tabela2.id AND tabela1.id = tabela2.id IN ('valor1', 'valor2') " +
+    "JOIN tabela3 ON tabela2.id = tabela3.id IN (SELECT * FROM tabela1) " +
+    "JOIN tabela4 ON tabela3.id = tabela4.id AND tabela1.id > 10 " +
+    "WHERE tabela1.id = tabela2.id AND tabela1.id = tabela2.id " +
+    "WHERE tabela2.id = tabela3.id AND tabela1.id > 30;"
 )
