b40422795a8deadf9e6aa53c2db4744a65ecead1
JLMSC <reecaptcha@gmail.com>
2023-05-03 15:39:03 -0300

    Adicionado exemplo de uso no main.py

--- source/main.py
+++ source/main.py
@@ -1,12 +1,19 @@
-from Parser.parser import Parser
+"""Arquivo principal.
+"""
+import Examples
 
-Parser(
-    "SELECT * " +
-    "FROM tabela1 " +
-    "JOIN tabela5 ON id = id " +
-    "JOIN tabela2 ON tabela1.id = tabela2.id AND tabela1.id = tabela2.id IN ('valor1', 'valor2') " +
-    "JOIN tabela3 ON tabela2.id = tabela3.id IN (SELECT * FROM tabela1) " +
-    "JOIN tabela4 ON tabela3.id = tabela4.id AND tabela1.id > 10 " +
-    "WHERE tabela1.id = tabela2.id AND tabela1.id = tabela2.id " +
-    "WHERE tabela2.id = tabela3.id AND tabela1.id > 30;"
+# pylint: disable=import-error
+from Parser.parser import Parser
+from RelationalAlgebra.Converter import Converter
+
+ph = Parser(
+    "SELECT nome, datanascimento, descricao, saldoinicial " +
+    "FROM usuario " +
+    "JOIN contas ON usuario.idusuario = contas.usuario_idusuario " +
+    "WHERE saldoinicial >= 235 AND uf = 'ce' AND cep <> '62930000' " +
+    "JOIN movimentacao ON usuario.idUsuario = movimentacao.idmovimentacao;"
 )
+ph.check_database_compatibility(database=Examples.pagamento_example_db)
+
+cnv = Converter(ph)
+print(cnv.convert_in_database_context(Examples.pagamento_example_db))
